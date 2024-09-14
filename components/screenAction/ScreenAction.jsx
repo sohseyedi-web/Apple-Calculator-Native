a03748a7c41905d46a979d594e9b11528ac7398e
sohseyedi <sohseyedi@gmail.com>
2024-09-14 09:49:13 +0330

    create screen action for show result calculator

---
+++ components/screenAction/ScreenAction.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ScreenAction = () => {
+  return (
+    <View>
+      <Text>ScreenAction</Text>
+    </View>
+  )
+}
+
+export default ScreenAction
+
+const styles = StyleSheet.create({})
