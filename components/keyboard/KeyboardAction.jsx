3d2dbd02ad3204d71400427b0c15104e29717ecd
sohseyedi <sohseyedi@gmail.com>
2024-09-14 11:11:18 +0330

    keyboad action and handle custom button

--- components/keyboard/KeyboardAction.jsx
+++ components/keyboard/KeyboardAction.jsx
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, useWindowDimensions, View } from "react-native";
+import React from "react";
+import BtnAction from "./BtnAction";
 
 const KeyboardAction = () => {
+  const { width } = useWindowDimensions();
+
+  const btnWidth = width / 4 - 5;
+
   return (
-    <View>
-      <Text>KeyboardAction</Text>
+    <View style={styles.keyboard}>
+      <View style={styles.btnRow}>
+        <BtnAction title={"AC"} w={btnWidth} bg="#a5a5a5" textColor={"#000"} />
+        <BtnAction title={"+/-"} w={btnWidth} bg="#a5a5a5" textColor={"#000"} />
+        <BtnAction title={"%"} w={btnWidth} bg="#a5a5a5" textColor={"#000"} />
+        <BtnAction title={"÷"} w={btnWidth} bg="#ff9f0c" />
+      </View>
+      <View style={styles.btnRow}>
+        <BtnAction title={"7"} w={btnWidth} />
+        <BtnAction title={"8"} w={btnWidth} />
+        <BtnAction title={"9"} w={btnWidth} />
+        <BtnAction title={"×"} w={btnWidth} bg="#ff9f0c" />
+      </View>
+      <View style={styles.btnRow}>
+        <BtnAction title={"4"} w={btnWidth} />
+        <BtnAction title={"5"} w={btnWidth} />
+        <BtnAction title={"6"} w={btnWidth} />
+        <BtnAction title={"-"} w={btnWidth} bg="#ff9f0c" />
+      </View>
+      <View style={styles.btnRow}>
+        <BtnAction title={"1"} w={btnWidth} />
+        <BtnAction title={"2"} w={btnWidth} />
+        <BtnAction title={"3"} w={btnWidth} />
+        <BtnAction title={"+"} w={btnWidth} bg="#ff9f0c" />
+      </View>
+      <View style={styles.btnRow}>
+        <BtnAction title={"0"} zero={false} w={width / 2 - 10} h={btnWidth} />
+        <BtnAction title={"."} w={btnWidth} />
+        <BtnAction title={"="} w={btnWidth} bg="#ff9f0c" />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default KeyboardAction
+export default KeyboardAction;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  keyboard: {
+    paddingBottom: 20,
+    paddingTop: 10,
+    paddingHorizontal: 10,
+  },
+  btnRow: {
+    flexDirection: "row",
+  },
+});
