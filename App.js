151f8c0d23c7ff3397572396952cf86ee890940f
sohseyedi <sohseyedi@gmail.com>
2024-09-14 09:45:27 +0330

    container app

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
-import KeyboardAction from "./components/KeyboardAction";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center bg-[#F1F2F3]">
-      <StatusBar style="dark" />
-      <KeyboardAction />
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+});
