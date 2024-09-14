7ed1e6591b2f2dc1b5f0e26643433cf575d63c01
sohseyedi <sohseyedi@gmail.com>
2024-09-14 09:47:48 +0330

    adding two component for start project

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import ScreenAction from "./components/screenAction/ScreenAction";
+import KeyboardAction from "./components/keyboard/KeyboardAction";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
+      <ScreenAction />
+      <KeyboardAction />
     </SafeAreaView>
   );
 }
