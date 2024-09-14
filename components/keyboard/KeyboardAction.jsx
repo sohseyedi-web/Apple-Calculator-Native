629503cd770ce58e5eb444b2d7480a31acdef8b8
sohseyedi <sohseyedi@gmail.com>
2024-09-14 09:48:19 +0330

    create keyboard action for buttons

---
+++ components/keyboard/KeyboardAction.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const KeyboardAction = () => {
+  return (
+    <View>
+      <Text>KeyboardAction</Text>
+    </View>
+  )
+}
+
+export default KeyboardAction
+
+const styles = StyleSheet.create({})
